754a8c5ede82dfa2b17f755580ef4aaa38f6f9a3
Samir Jaber <samirjaber50@gmail.com>
2021-03-12 11:35:14 -0500

    Added a character counter & limit with a portion of the "MyProfile" feature

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var tweetTextView: UITextView!
-    
+    @IBOutlet weak var characterCounter: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+
+        self.updateCharacterCount()
         // Do any additional setup after loading the view.
     }
     
+    func updateCharacterCount() {
+        let charCount = self.tweetTextView.text.count
+        
+        self.characterCounter.text = "\((0) + charCount)/280"
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        self.updateCharacterCount()
+    }
    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let characterLimit = 280
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+                
+        return newText.count <= characterLimit
+    }
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
